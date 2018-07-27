ac977e4c12be711565e05592ffbd38286457f20e
Daniel Escobar <escobard90@gmail.com>
2018-07-26 20:03:54 -0600

    part2 - notes: lesson 5, lecture 9-10 notes - block encrypted and previous hash logic implemented

--- part2/project2/simpleChain.js
+++ part2/project2/simpleChain.js
@@ -1,5 +1,13 @@
 // navigate to this folder and type the `node` command, then copy and pase
 // the code below into the terminal node.js runtime
+// need to remove all commentary by going:
+// control find = //, select all
+// shift + end, selects all lines
+
+// each class and constant must be entered one at a time in the console to work
+
+// loads the SHA256 library to encrypt - SO COOL
+const SHA256 = require('crypto-js/sha256');
 
 // this holds the block, anything passed into the class argument
 // is fed into the data property within the constructor
@@ -10,7 +18,7 @@ class Block{
 	constructor(data){
    
    // sets these as global variables to the class, much like state
-   // contains the block properties
+   // contains the header block properties
    this.hash = "",
    this.height = 0,
    // passes the transaction hashes to the this.body property.
@@ -30,11 +38,32 @@ class Blockchain{
 	// creates the constructor that will hold all chain data
 	constructor(){
 		this.chain = [];
+
+		// ensures the blockchain always contains at least a single block
+		// this is commonly refered to as the genesis block, as its the first block of the blockchain
+		this.addBlock(new Block("Genesis block - First block in the chain"))
 	}
 
 	// adds a block to the chain constructor, effectively creating a blockchain!
 	addBlock(newBlock){
-		this.chain.push(newBlock);
+
+		let { chain } = this
+		// checks to see if the chain contains more than one block, to grab the previous block's hash
+		if (chain.length > 0 ) {
+
+			// sets the property of the new block as the previous block's hash
+			newBlock.previousblockhash = 
+
+				// searches the block prior to this one via the array's index
+				// then grabs the hash from the .hash property of the previous block
+				chain[chain.length-1].hash;
+		}
+
+		// this encrypts our block data with a SHA256 algorythm, encrypting the block!!
+		newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+
+		// each block is finally pushed into the chain global constructor, creating our blockchain!
+		chain.push(newBlock);
 	}
 
 }
