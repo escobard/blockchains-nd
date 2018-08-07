41dd9dcf95827796fd589312720e58d060da2fcb
Daniel Escobar <escobard90@gmail.com>
2018-08-07 17:32:45 -0600

    feat: added README.md to meet requirements, added route info

--- part3/routes/index.js
+++ part3/routes/index.js
@@ -4,6 +4,8 @@ const routes = require('../constants/routes');
 
 module.exports = (app) => {
 
+  app.use(routes.getBlockHeight, require('../routes/getBlockHeight'));
+  app.use(routes.postBlock, require('../routes/postBlock'));
   app.use(routes.main, require('../routes/main'));
   app.use(routes.health, require('../routes/health'));
   app.use(routes.addBlock, require('../routes/addBlock'));
