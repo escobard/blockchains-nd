ae7fb7bffe712cd215b3b7d59df4e015ae4b449f
Daniel Escobar <escobard90@gmail.com>
2018-08-22 16:25:33 -0600

    project4 - created middleware for validation checks

--- part3/project4/middlewares/checkSignature.js
+++ part3/project4/middlewares/checkSignature.js
@@ -2,7 +2,9 @@ module.exports = async (req, res, next) => {
 
 	// passed middleware logic once the global signature variable exists for validation
 	if (signature) {
-		console.log("Validation time window open, proceeding to route...");
+
+		// consider creating some logic to handle the plurality of the minute(s) text
+		console.log(`Validation time window open for ${authWindow} minutes, proceeding to route...`);
 		next();
 	} else {
 		res.status(401).json({
