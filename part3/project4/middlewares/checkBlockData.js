37f67a0df2767aefa664452907e20aa5c847a013
Daniel Escobar <escobard90@gmail.com>
2018-08-26 19:14:54 -0600

    project4 - doc: updated documentation for checkBlockData, most functionality is complete!

--- part3/project4/middlewares/checkBlockData.js
+++ part3/project4/middlewares/checkBlockData.js
@@ -10,26 +10,31 @@ module.exports = async (req, res, next) => {
 		// checks to ensure star.story is hex encoded
 		let hex = checkHex(star.story);
 		if (hex) {
+			console.log("Valid hex, checking ascii characters...");
 			// converts story from hex to asii
 			let convertAscii = hexToAscii(star.story);
 
 			// checks if converted string contains only ascii characters
 			let ascii = checkAscii(convertAscii);
 			if (ascii) {
+				console.log("Valid characters, checking story word length...");
 				// checks total word count of string
 				let count = wordCount(convertAscii);
 
 				// ensures story word count is less than 250
 				if (count <= 250) {
 					// final validation, block data valid!
-					next()
+					console.log("Success, block data is valid, creating new block...");
+					next();
 				} else {
+					console.log("Failed, star.story contains more than 250 words!");
 					res.status(401).json({
-						status: "Failed, star.story contains invalid ascii characters!",
+						status: "Failed, star.story contains more than 250 words!",
 						authenticated: false
 					});
 				}
 			} else {
+				console.log("Failed, star.story contains invalid ascii characters!");
 				res.status(401).json({
 					status: "Failed, star.story contains invalid ascii characters!",
 					authenticated: false
@@ -42,6 +47,7 @@ module.exports = async (req, res, next) => {
 			});
 		}
 	} else {
+		console.log("Failed, invalid post object values");
 		res.status(401).json({
 			status: "Failed, invalid post object values",
 			authenticated: false
