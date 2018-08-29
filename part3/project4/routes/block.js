31fcf80965e4e8fab2493563353e7499ff2126a2
Daniel Escobar <escobard90@gmail.com>
2018-08-28 20:20:54 -0600

    project4 - feat: destroying global routes, forcing re-auth on post

--- part3/project4/routes/block.js
+++ part3/project4/routes/block.js
@@ -3,30 +3,40 @@
 const router = require("express").Router();
 
 let blockchain = require("../services/blockchain"),
-initChain = require('../middlewares/initChain'),
-checkValidation = require('../middlewares/checkValidation'),
-checkBlockData = require('../middlewares/checkBlockData')
+  initChain = require("../middlewares/initChain"),
+  checkValidation = require("../middlewares/checkValidation"),
+  checkBlockData = require("../middlewares/checkBlockData");
 
 // the blockHeight route parameter is expted here, and passed back to the route
-router.post("/", initChain, checkValidation, checkBlockData, async (req, res) => {
-  let { headers, params, body } = req;
-  
-        // adds block data based on route parameters
-        blockchain.addBlock(body);
-        let chain = await blockchain.fetchBlockchain(); 
-          // sets the blockchain service data with data from leveldb
-          blockchain.chain = chain;
-          let newBlock = blockchain.getBlock(blockchain.height);
-          // logs the blockchain
-          console.log("request:", body);
-          console.log("New Block:", newBlock);
+router.post(
+  "/",
+  initChain,
+  checkValidation,
+  checkBlockData,
+  async (req, res) => {
+    let { headers, params, body } = req;
 
-          res.send({
-            healthy: true,
-            responseData: `added new block with the following data: ${body}`,
-            newBlock,
-            blockchain
-          });
-});
+    // adds block data based on route parameters
+    blockchain.addBlock(body);
+    let chain = await blockchain.fetchBlockchain();
+    // sets the blockchain service data with data from leveldb
+    blockchain.chain = chain;
+    let newBlock = blockchain.getBlock(blockchain.height);
+    // logs the blockchain
+    delete global.address;
+    delete global.authenticated;
+    delete global.signature;
+    delete global.authWindow;
+
+    console.log("Destroyed all global variables");
+
+    res.send({
+      healthy: true,
+      responseData: `added new block with the following data: ${body}`,
+      newBlock,
+      blockchain
+    });
+  }
+);
 
 module.exports = router;
