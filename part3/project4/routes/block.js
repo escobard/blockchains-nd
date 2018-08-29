8f8ad520ecfcf628e45bd97c1260762af5bbe06a
Daniel Escobar <escobard90@gmail.com>
2018-08-29 12:44:00 -0600

    project4 - refactor: refactored getHeight route to /block route handler

--- part3/project4/routes/block.js
+++ part3/project4/routes/block.js
@@ -39,4 +39,31 @@ router.post(
   }
 );
 
+// the blockHeight route parameter is expted here, and passed back to the route
+router.get('/:blockHeight', initChain,  async (req, res) => {
+  let { params: { blockHeight } } = req;
+  let block = blockchain.getBlock('height', blockHeight);
+  // blockchain.validateBlock(blockHeight);
+  // console.log('request: ', headers)
+  // console.log('request parameters: ', params)
+  console.log("block: ", block);
+
+
+  // if block does not exist, return different response
+  if (!block) {
+    res.send(
+    {
+      healthy: true,
+      blockHeightParams: blockHeight,
+      block: 'Block does not exist - check the /getBlockHeight to check current chain height'
+    });
+  }
+
+  res.send(
+    {
+      blockHeightParams: `Height: ${blockHeight}`,
+      block,
+    });
+});
+
 module.exports = router;
--- part3/project4/routes/index.js
+++ part3/project4/routes/index.js
@@ -6,15 +6,13 @@ module.exports = (app) => {
 
 	// project 3 routes
   app.use(routes.main, require('../routes/main'));
-  app.use(routes.block, require('../routes/block'));
   app.use(routes.health, require('../routes/health'));
-  app.use(routes.notary, require('../routes/notary'));
   app.use(routes.addBlock, require('../routes/addBlock'));
-  app.use(routes.getBlock, require('../routes/getBlock'));
   app.use(routes.postBlock, require('../routes/postBlock'));
   app.use(routes.getHeight, require('../routes/getBlockHeight'));
 
   // project 4 routes
   app.use(routes.requestValidation, require('../routes/requestValidation'))
   app.use(routes.messageSignature, require('../routes/messageSignature'))
+  app.use(routes.block, require('../routes/block'));
 };
